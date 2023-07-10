d7c91afb7c17e3ce9625cda885e2fae6f5aad370
meel516 <md.saleem516@gmail.com>
2023-07-10 05:47:38 +0530

    added mongo db check

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ let port =6000
 app.listen(port,()=>console.log('listening on port'+port))
 app.post('/connect',(req,res)=>{
     console.log(req.body)
-    res.json({status:true,data:{token:'free4all',rng:9999999999999999}})
+    connection.find({user_key: req.body.user_key},(error,users)=>{
+        if(error){
+            res.json({status:false,reason:'user is not registered'})
+        }
+       if(users){
+        res.json({status:true,data:{token:'free4all',rng:users.expiry,modname:users.modname,username:users.username}})
+       }
+    })
+  
 })
 app.post('/createnewuser',(req,res)=>{
     newuser=new connection({
