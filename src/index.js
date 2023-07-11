284733ab48e0e573be43180241cfa6bfdff9629a
meel516 <md.saleem516@gmail.com>
2023-07-11 05:45:25 +0530

    added

--- src/index.js
+++ src/index.js
@@ -21,10 +21,13 @@ app.post('/connect',(req,res)=>{
     console.log(req.body)
     connection.findOne({key: req.body.user_key}).then((data)=>{
         console.log(data)
-      if(data==null){
+      if(data==null||req.body.serial!=data.serial){
             res.json({status:false,reason:'user is not registered'})
       }
       else{
+        if(data.serial==''){
+          connection.findOneAndUpdate({key:req.body.user_key},{serial:data.serial})
+        }
         res.json({status:true,data:{token:'free4all',rng:parseInt(data.expiry),modname:data.modname,username:data.username}})
       }
     }
@@ -39,7 +42,8 @@ app.post('/protected/createnewuser',(req,res)=>{
         username: data.username,
     key: req.body.key,
     modname: req.body.modname,
-    expiry: parseInt(req.body.expiry)
+    expiry: parseInt(req.body.expiry),
+    serial:''
     })
     newuser.save().then((dta)=>console.log(dta)).catch((err)=>console.log(err))
     res.json({status:"key created successfully"})
